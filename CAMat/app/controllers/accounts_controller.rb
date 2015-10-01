312d55915f4878e9b1c140bb12c12afcdaea4292
msart <pedromarcondes94@hotmail.com>
2015-10-01 14:28:31 -0300

    Account done/error treatment

--- CAMat/app/controllers/accounts_controller.rb
+++ CAMat/app/controllers/accounts_controller.rb
@@ -3,7 +3,7 @@ class AccountsController < ApplicationController
   def create
     @account = Account.create!(locker: params[:locker], user: params[:user], expire_date: params[:expire_date])
     Locker.find_by_code(params[:locker]).update_attributes! owner: params[:user]
-    flash[:notice] = "Armario obtido com sucesso! #{Account.all}"
+    flash[:notice] = "Armario obtido com sucesso!"
     redirect_to users_path
   end
 end
