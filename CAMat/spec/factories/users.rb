8ff5faa7537f51ab7f71f31d492659d5b6cf13dd
focmoreira@gmail.com <root@cpro28564.publiccloud.com.br>
2015-10-09 05:20:18 -0300

    Adicionado testes ao user, faltando testes de metodo e teste do caso que o user nao eh admin

--- CAMat/spec/factories/users.rb
+++ CAMat/spec/factories/users.rb
@@ -1,15 +1,19 @@
 FactoryGirl.define do
-  factory :userAdmin do |u|
-    u.nome { Faker::Name.name }
-    u.password{ Faker::Base.bothify}
+  factory :user do |u|
+    u.nome { "Pedro Marcondes" }
+    u.password { "abc123"}
+    u.password_confirmation { "abc123"}
     
-    u.email { Faker::Internet.email }
-    u.documento{ Faker::Number.number } 
-    u.telefone { Faker::Number.number }   
+    u.email { "emailPedro@site.com.br" }
+    u.documento{ "8564712" } 
+    u.telefone { "11147820" }   
 
-    u.admin = TRUE
+    u.admin true
   end
   
+  
+  
+=begin  
     factory :userNormal do |u|
     u.nome { Faker::Name.name }
     u.password{ Faker::Base.bothify}
@@ -18,7 +22,8 @@ FactoryGirl.define do
     u.documento{ Faker::Number.number } 
     u.telefone { Faker::Number.number }   
 
-    u.admin = FALSE
+    u.admin false
   end
-  
+=end
 end
+
--- CAMat/spec/models/user_spec.rb
+++ CAMat/spec/models/user_spec.rb
@@ -1,15 +1,49 @@
 require 'rails_helper'
 
 describe User do
+  
   before :each do
-    @userAdmin = FactoryGirl.create(:userAdmin)
-    @userNormal = FactoryGirl.create(:userNormal)
+    @user = FactoryGirl.create(:user)
   end
-  it "is invalid without a nome"
-  it "is invalid without a telefone"
-  it "is invalid without a email"
-  it "is invalid without a documento"
-  it "is invalid withou a password"
-  it "is a admin" 
-  it "is not a admin"
+  
+  
+  it "has a valid factory" do 
+      expect(@user).to be_valid
+  end
+  it "is invalid without a nome" do
+    expect(@user.nome ).to match(/[a-zA-z\s]+/)
+  end
+  
+  it "is invalid without a telefone" do
+    expect(@user.telefone ).to match(/^\d*$/)
+  end
+  
+  it "is invalid without a email" do
+   expect(@user.email).to match(/[\w*]+@+[\w*]+[.]+.*/)
+  end
+
+  it "is invalid without a documento" do
+    expect(@user.documento ).to match(/^\d*$/)
+  end
+
+  it "is invalid without a password" do
+    @user.password.present?
+  end
+
+  it "is a admin" do
+    expect(@user.admin ).to  be_truthy 
+  end
+  
+=begin  
+  it "is not a admin" do
+    expect(@userAdmin.admin ).to  be_false 
+    expect(@userNormal.documento ).to be_true
+  end
+  
+  it "has locker" do
+    expect(@userAdmin.locker ).to be_nil
+  end
+  
+  it "don't have locker"
+=end
 end
