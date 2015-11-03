f266abf2f6b7473c68cb873052fa745c83595723
saasbook <saasbook@saasbook.(none)>
2015-11-03 19:40:11 -0400

    giving up locker testes

--- CAMat/features/step_definitions/user_steps.rb
+++ CAMat/features/step_definitions/user_steps.rb
@@ -2,11 +2,39 @@ Given(/^I am at the user creation page$/) do
   visit new_user_path
 end
 
-Given(/^I have a User with nome "(.*?)" and email "(.*?)" and documento "(.*?)" and telefone"(.*?)" and password "(.*?)" and password_confirmation "(.*?)" and admin false$/) do |nome, email, documento, telefone, password, password_confirmation|
+Given(/^I have a User with nome "(.*?)" and email "(.*?)" and documento "(.*?)" and telefone "(.*?)" and password "(.*?)" and password_confirmation "(.*?)" and admin false$/) do |nome, email, documento, telefone, password, password_confirmation|
   User.create(nome: nome, email: email, documento: documento, telefone: telefone, password: password, password_confirmation: password_confirmation, admin: false)
+end
+
+Given(/^I have a valid user with name (.*?)$/) do |name|
+  User.create!(nome: name, email: "email@mail.com", documento: 1111111, telefone: 11111111, password: 11111111, password_confirmation: 11111111, admin: false)
 end
 
 Given(/^I am at the login page$/) do
   visit login_path
 end
 
+Given(/^I am at my home page$/) do
+  steps %Q{
+    Given I have a valid user with name Rafael 
+  }
+  visit user_path(id: User.find_by_nome("Rafael")) 
+end
+
+Then(/^I should see a confirmation window$/) do
+  steps %Q{
+    Then I should see "Esta certo de que quer abandonar seu armario?" 
+  }
+  
+end
+
+Then(/^I should be at my home page$/) do
+  steps %Q{
+    Then I should see "Requerir Armario" 
+  }
+  
+end
+
+Then(/^I should not see "(.*?)"$/) do |arg1|
+  expect(page).not_to have_content(text)
+end
--- CAMat/features/user/giving_up_locker.feature
+++ CAMat/features/user/giving_up_locker.feature
@@ -1,18 +1,18 @@
-Feature: User fiving up his locker
-  In order to free my locker that i dont use anymore
+Feature: User giving up his locker
+  In order to free my locker, which I won`t use anymore
   As a client
   I want to be able to give up my locker
 
   Scenario: While I have a locker
-    Given I am at the my home page
-    When I press "Abandonar Armário"
-    Then I should see a confirmation windown
-    When I press the confirm button
+    Given I am at my home page
+    When I press the "Abandonar Armário" button
+    Then I should see a confirmation window
+    When I press the "Sim" button
     Then I should be at my home page
     And I should see "Você abandonou seu armário"
     And I should see "Armário:-"
 
   Scenario: While I have no locker
-    Given I am at the my home page
+    Given I am at my home page
     Then I should not see "Abandonar Armário"
     
