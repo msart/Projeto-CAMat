a738cc75f53d1a17879afb9a74160130975bf7f2
saasbook <saasbook@saasbook.(none)>
2015-11-04 07:33:13 -0400

    trabalhando nos testes de admin

--- CAMat/features/step_definitions/user_steps.rb
+++ CAMat/features/step_definitions/user_steps.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 Given(/^I am at the user creation page$/) do
   visit new_user_path
 end
@@ -21,10 +22,25 @@ end
 Then(/^I should not see "(.*?)"$/) do |arg1|
   expect(page).not_to have_content(text)
 end
-Given(/^I have a User with nome "(.*?)" and email "(.*?)" and documento "(.*?)" and telefone"(.*?)" and password "(.*?)" and password_confirmation "(.*?)" and admin true/) do |nome, email, documento, telefone, password, password_confirmation|
+
+Given(/^I have a User with nome "(.*?)" and email "(.*?)" and documento "(.*?)" and telefone"(.*?)" and password "(.*?)"/) do |nome, email, documento, telefone, password|
   User.create(nome: nome, email: email, documento: documento, telefone: telefone, password: password, password_confirmation: password_confirmation, admin: false)
 end
 
+Given(/^I have an Admin with nome "(.*?)" and email "(.*?)" and documento "(.*?)" and telefone "(.*?)" and password "(.*?)"$/) do |nome, email, documento, telefone, password|
+  User.create(nome: nome, email: email, documento: documento, telefone: telefone, password: password, password_confirmation: password, admin: true)
+end
+
+Given(/^I am logged in as an Admin$/) do
+  steps %Q{
+    Given I have an Admin with nome "admin" and email "admin@camat.com" and documento "1212121" and telefone "12121212" and password "senhaadmin"
+    Given I am at the login page 
+  }
+  fill_in "Email", :with => "admin@camat.com"
+  fill_in "Senha", :with => "senhaadmin"
+  click_button "Mostrar Usuário"
+  visit user_path(User.find_by_nome("admin"))
+end
 
 Given (/^the following user exist:$/) do |user_table|
   user_table.hashes.each do |user|
--- CAMat/features/user/admin_editing.feature
+++ CAMat/features/user/admin_editing.feature
@@ -1,17 +1,16 @@
 Feature: Admin editing permission
-	In order to edit users information
+  In order to edit users information
   As a admin user
   I want to be able to acess and edit users information
 
   Scenario: access to user list
-   	Given I have a User with nome "Admin" and email "admin@camat.com" and documento "1234" and telefone"1234546" and password "123456" and password_confirmation "123456" and admin true
+   	Given I am logged in as an Admin
+    When I press the "Voltar aos usuários" button
+    Then I should see "Índice de Usuários"
     #terminar
-
-
-
 
   Scenario: editing a generic user
-    Given I have a User with nome "Admin" and email "admin@camat.com" and documento "1234" and telefone"1234546" and password "123456" and password_confirmation "123456" and admin true
-    And I have a User with nome "Clientenome" and email "cliente@cliente.com" and documento "1234" and telefone"1234546" and password "123456" and password_confirmation "123456" and admin false
-    #terminar
+    Given I have an Admin with nome "Admin" and email "admin@camat.com" and documento "1234" and telefone "1234546" and password "123456"
+    And I have a User with nome "Clientenome" and email "cliente@cliente.com" and documento "1234" and telefone "1234546" and password "123456"
+    #termne
 
