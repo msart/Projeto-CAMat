56cc263557ff56d11d0d3dcaab2b159fa2b36a05
msart <pedromarcondes94@hotmail.com>
2015-11-09 20:34:46 -0200

    seed, arrumado

--- CAMat/app/models/raffle.rb
+++ CAMat/app/models/raffle.rb
@@ -14,12 +14,12 @@ class Raffle < ActiveRecord::Base
     end
     this.destroy
   end
-  handle_asynchronously :run_raffle, :run_at => finish.to_time
+  #handle_asynchronously :run_raffle, :run_at => finish.to_time
   
   def run_raffle(locker)
     subscribed_users = this.users.shuffle
     Account.create locker: locker, user: subscribed_users.pop, expire_date: 1.year.from_now
     this.destroy
   end
-  handle_asynchronously :run_raffle, :run_at => finish.to_time
+ # handle_asynchronously :run_raffle, :run_at => finish.to_time
 end
--- CAMat/db/seeds.rb
+++ CAMat/db/seeds.rb
@@ -1,10 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-    Locker.create(owner: 'usuario1', code: 'A1')
-    Locker.create(owner: '', code: 'B7')
-    User.create(email: 'admin@camat.com', nome: 'administradorraiz', documento: '1', telefone:'11111', password: '123456', password_confirmation: '123456', admin: true)
+	for l in 'A'..'U' do
+	  	unless l != 'K'
+	  	else
+	    	for i in 1..16 do
+	    	  	Locker.create(owner: '', code: "#{l}#{i.to_s.rjust(2, '0')}")
+	    	end
+	    end
+	end
+	User.create(email: 'admin@camat.com', nome: 'administradorraiz', documento: '1', telefone:'11111', password: '123456', password_confirmation: '123456', admin: true)
