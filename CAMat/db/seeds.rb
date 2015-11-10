d7a1ec7b00203fd3b7d3e5aa700f388d5687dcaf
msart <pedromarcondes94@hotmail.com>
2015-11-10 19:10:09 -0200

    parte 2 final

--- CAMat/db/seeds.rb
+++ CAMat/db/seeds.rb
@@ -8,3 +8,4 @@
 	    end
 	end
 	User.create(email: 'admin@camat.com', nome: 'administradorraiz', documento: '1', telefone:'11111', password: '123456', password_confirmation: '123456', admin: true)
+	Raffle.create(start:  DateTime.parse('21/01/2016'), finish:  DateTime.parse('25/01/2016'))
