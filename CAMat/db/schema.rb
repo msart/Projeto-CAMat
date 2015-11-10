1b1a6b1ac5592cb18bce217c6b3169d8815749e8
msart <pedromarcondes94@hotmail.com>
2015-11-10 13:06:05 -0200

    varios errinhos arrumados

--- CAMat/db/schema.rb
+++ CAMat/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151109173741) do
+ActiveRecord::Schema.define(:version => 20151107232811) do
 
   create_table "accounts", :force => true do |t|
     t.string   "user"
@@ -35,7 +35,18 @@ ActiveRecord::Schema.define(:version => 20151109173741) do
     t.datetime "updated_at", :null => false
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'belongs_to' for column 'raffle'
+  create_table "users", :force => true do |t|
+    t.string   "nome"
+    t.string   "email"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
+    t.string   "documento"
+    t.string   "telefone"
+    t.boolean  "admin"
+  end
+
+  add_index "users", ["documento"], :name => "index_users_on_documento", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
