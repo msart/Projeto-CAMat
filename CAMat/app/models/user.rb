8699a08552a2663b78850697e8c2af72abbd7a8c
saasbook <saasbook@saasbook.(none)>
2015-12-09 00:59:36 -0400

    fazendo sorteio do requerimento

--- CAMat/app/models/user.rb
+++ CAMat/app/models/user.rb
@@ -1,7 +1,8 @@
 # -*- encoding : utf-8 -*-
 class User < ActiveRecord::Base
   attr_accessible :email, :nome, :documento, :telefone, :password, :password_confirmation, :admin
-  belongs_to :raffle, :requirement_raffle
+  belongs_to :raffle
+  belongs_to :requirement_raffle
   validates :nome, presence: true, length: { maximum: 50 }
 
   before_save { self.email = email.downcase } 
