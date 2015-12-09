1608733b21dcd1eb5e650aaadf9e76251bf73366
msart <pedromarcondes94@hotmail.com>
2015-12-09 02:24:21 -0200

    hotfix migration

--- CAMat/db/migrate/20151209015749_add_foreign_requeriment_raffle.rb
+++ CAMat/db/migrate/20151209015749_add_foreign_requeriment_raffle.rb
@@ -1,9 +1,9 @@
 # -*- encoding : utf-8 -*-
 class AddForeignRequerimentRaffle < ActiveRecord::Migration
 
-  def change
-    add_column :requeriment_raffles, :user_id, :integer
-    add_column :users, :requeriment_raffle_id, :integer
+  def up
+    add_column :requirement_raffles, :user_id, :integer
+    add_column :users, :requirement_raffle_id, :integer
   end
 
 end
--- CAMat/db/schema.rb
+++ CAMat/db/schema.rb
@@ -1,4 +1,4 @@
-# -*- encoding : utf-8 -*-
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151207234738) do
+ActiveRecord::Schema.define(:version => 20151209015749) do
 
   create_table "accounts", :force => true do |t|
     t.string   "user"
@@ -58,16 +58,26 @@ ActiveRecord::Schema.define(:version => 20151207234738) do
     t.integer  "user_id"
   end
 
+  create_table "requirement_raffles", :force => true do |t|
+    t.datetime "finish"
+    t.datetime "start"
+    t.string   "locker"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "user_id"
+  end
+
   create_table "users", :force => true do |t|
     t.string   "nome"
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
     t.string   "password_digest"
     t.string   "documento"
     t.string   "telefone"
     t.boolean  "admin"
     t.integer  "raffle_id"
+    t.integer  "requirement_raffle_id"
   end
 
   add_index "users", ["documento"], :name => "index_users_on_documento", :unique => true
