261fcdf2941c3e05dd0e5ed4bc16714ccd309191
Gustavo Cayres <gustavocayrespublico@gmail.com>
2016-12-06 00:56:57 -0200

    Implement DESIGN PATTERN Strategy in locker model

--- CAMat/app/controllers/lockers_controller.rb
+++ CAMat/app/controllers/lockers_controller.rb
@@ -6,6 +6,10 @@ class LockersController < ApplicationController
   
   def occupied_lockers_list
     @lockers = Locker.all
+    @lockers.collect! do |locker|
+      locker.set_strategy
+      locker
+    end
     @time = DateTime.now
   end
 end
--- CAMat/app/models/locker.rb
+++ CAMat/app/models/locker.rb
@@ -2,28 +2,45 @@
 class Locker < ActiveRecord::Base
   attr_accessible :code, :owner
   
-  def self.locker_occupation_hash
-    locker_occupation_hash = Hash.new('')
-    Locker.all.each do |locker|
-    	locker_occupation_hash[locker.code] = locker.is_occupied?
-    end
-    
-    return locker_occupation_hash
+  attr_writer :strategy
+  attr_reader :strategy
+  
+  def set_strategy
+  	if(self.number < 9)
+	 	  self.strategy = LowerLockerPricingStrategy.new(20)
+		else
+			self.strategy = UpperLockerPricingStrategy.new(20)
+  	end
   end
   
   def price
-		locker_number = 2
-		price = 0
-		if(locker_number < 9)
-	 	  price = 20	# price strategy 1
-		else
-			price = 30 # price strategy 2
-		end
-		return price 
+		return self.strategy.price 
   end
   
   def free?
   	return owner.to_s == ""
   end
+  
+  def number
+  	return (code[1]+code[2]).to_i
+  end
+end
+
+class LockerPricingStrategy
+  def initialize(price_in_br)
+    @base_price = price_in_br
+  end
+end
+
+class LowerLockerPricingStrategy < LockerPricingStrategy
+  def price
+    return "R$#{@base_price},00"
+  end
+end
+  
+class UpperLockerPricingStrategy < LockerPricingStrategy
+  def price
+    return "R$#{@base_price + 10},00" 
+  end  
 end
 
